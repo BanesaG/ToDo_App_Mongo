f04142e0ef26eb9b58464c09be265f7ebc8658b9
BanesaG <42129826+BanesaG@users.noreply.github.com>
2018-11-15 21:52:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
